3e8e7e9999afd6499cb8b8a73dad18d3e4151527
Dmitry Alexandrov <d.aleksandrov@itigris.ru>
2020-10-29 23:45:30 +0300

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM openjdk:11-jdk-slim as build-stage
+FROM openjdk:11-jdk-slim
 WORKDIR /app
 COPY build/libs/*.jar /app/datagrid.jar
 
 ENTRYPOINT ["java", "-jar", "/app/datagrid.jar"]
-
--- src/main/java/me/progbloom/datagrid/DatagridApplication.java
+++ src/main/java/me/progbloom/datagrid/DatagridApplication.java
@@ -3,10 +3,8 @@ package me.progbloom.datagrid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:db/datasource/development.properties")
 @EnableDiscoveryClient
 public class DatagridApplication {
 
